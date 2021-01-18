d0a0b06b63c5973ba855687204c282d775dfe0ca
ShipilA <44925533+ShipilA@users.noreply.github.com>
2021-01-18 14:21:30 +0300

    Add files via upload

---
+++ Factorials/PeerReview5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PeerReview5
+{
+    class Fractal
+    {
+        int repeat = 10, startColor = 0, endColor = 0, len;
+
+        public void DrawFractal()
+        {
+
+        }
+    }
+
+    class FractalTree : Fractal
+    {
+        double k, angle1, angl2;
+    }
+    class CochCurve : Fractal
+    {
+
+    }
+    class SierpinskiCarpet : Fractal
+    {
+
+    }
+    class SierpinskiTriangle : Fractal
+    {
+
+    }
+    class CantorQuantity : Fractal
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
